936f43828a92ed7ec0b2d9958586dd70deb469a5
Junwei Luo <18096336967@163.com>
2025-04-24 16:01:35 +0800

    本次提交完成了项目SpringBoot环境的搭建-commit03

---
+++ xshopping/src/main/java/com/javaclimb/xshopping/XshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.javaclimb.xshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XshoppingApplication.class, args);
+    }
+
+}
